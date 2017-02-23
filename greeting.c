bd362ca165cbe01e5c745637b2df647a928a11a4
OleaBlossom <o.corcoran@gmail.com>
2017-02-23 11:09:28 +1100

    Create greeting.c
    Variations on Hello World

---
+++ greeting.c
@@ -0,0 +1,12 @@
+/* a "hello world" application */
+
+/* preprocessor instructions */
+#include <stdio.h>
+
+/* main program */
+int main()
+{
+	/* code */
+	printf("\\\\Welcome to \"C\" Programming//\n");
+	return 0;
+}
